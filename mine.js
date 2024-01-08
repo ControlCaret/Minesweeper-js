c3dad36ae38e9f706440a3fb9a36a9bff4c9f3c5
Heejae Kwak <controlcaret@gmail.com>
2024-01-08 16:30:43 +0900

    Implement bomb counter

--- mine.js
+++ mine.js
@@ -1,53 +1,47 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const grid = document.querySelector('.grid');
-    let width = 10;
-    let bombAmount = 20;
-    let flags = 0;
-    let cells = [];
-    let isGameOver = false;
+    const board = document.querySelector('.board');
+    const flagsLeft = document.querySelector('#flags-left');
+    const timer = document.querySelector('#timer');
+    const emojiBtn = document.querySelector('.emoji-btn');
 
-    // create Board
+    let width = 10;
+    let bombAmount = 5;
+    let flags = 0;
+    let squares = [];
+    let isGameOver = false;
+    let time = 0;
+
     function createBoard() {
         const bombsArray = Array(bombAmount).fill('bomb');
         const emptyArray = Array(width * width - bombAmount).fill('normal');
-        const gameArray = emptyArray.concat(bombsArray);
-        const shuffledArray = gameArray.sort(() => Math.random() - 0.5);
+        const tempArray = emptyArray.concat(bombsArray);
+        const shuffledArray = tempArray.sort(() => Math.random() - 0.5);
+
+        let cells = [];
 
         for (let i = 0; i < width * width; i++) {
-            const cell = document.createElement('div');
+            const cell = document.createElement('cell');
             cell.setAttribute('id', i);
             cell.classList.add(shuffledArray[i]);
-            grid.appendChild(cell);
+            board.appendChild(cell);
             cells.push(cell);
+        }
 
-            // left click
-            cell.addEventListener('click', function (e) {
-                //click(cell);
-            });
+        for (let i = 0; i < cells.length; i++) {
+            let bombCount = 0;
 
-            // right click
-            cell.oncontextmenu = function (e) {
-                e.preventDefault();
-                //addFlag(cell);
-            }
-
-            for (let i = 0; i < cells.length; i++) {
-                let total = 0;
-                const isLeftEdge = (i % width === 0);
-                const isRightEdge = (i % width === width - 1);
-
-                if (cells[i].classList.contains('cell', 'normal')) {
-                    if (i > 0 && !isLeftEdge && cells[i - 1].classList.contains('bomb')) total++;
-                    if (i > 9 && !isRightEdge && cells[i + 1 - width].classList.contains('bomb')) total++;
-                    if (i > 10 && cells[i - width].classList.contains('bomb')) total++;
-                    if (i > 11 && !isLeftEdge && cells[i - 1 - width].classList.contains('bomb')) total++;
-                    if (i < 98 && !isRightEdge && cells[i + 1].classList.contains('bomb')) total++;
-                    if (i < 90 && !isLeftEdge && cells[i - 1 + width].classList.contains('bomb')) total++;
-                    if (i < 88 && !isRightEdge && cells[i + 1 + width].classList.contains('bomb')) total++;
-                    if (i < 89 && cells[i + width].classList.contains('bomb')) total++;
-                    cells[i].setAttribute('data', total);
+            if (cells[i].classList.contains('normal')) {
+                for (let y = -1; y <= 1; y++) {
+                    for (let x = -1; x <= 1; x++) {
+                        const cell = cells[i + x + y * width];
+                        if (cell && cell.classList.contains('bomb')) {
+                            bombCount++;
+                        }
+                    }
                 }
             }
+            cells[i].setAttribute('data', bombCount);
+            cells[i].innerHTML = bombCount;
         }
     }
     createBoard();
